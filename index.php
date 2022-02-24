a3717129779d1461f4a5f06f5aabc3603aefdb28
SangesAntonio <sanges65@gmail.com>
2022-02-24 15:25:37 +0100

    created project

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$text = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas deleniti sed ea explicabo impedit! Vel in amet sunt repellat eius vero animi doloribus rerum explicabo aspernatur, optio nostrum recusandae quaerat.';
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1> <?php echo $text ?></h1>
+
+</body>
+
+</html>
